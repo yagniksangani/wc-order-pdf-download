yagniksangani/wc-order-pdf-download

commit fe7476100c3b243796d68942d0db4c688213a988

hook to add order item meta

includes/class-wc-order-pdf-download.php
@@ -163,8 +163,11 @@ class WC_Order_PDF_Download {
 
 		 	$html .= '<td><table cellpadding="5" cellspacing="0" border="1" style="border:1px dashed black;width:100%"><tr><td><strong>'.esc_html__( "Item Name", "wcorderpdf" ).'</strong></td><td><strong>'.esc_html__( "Quantity", "wcorderpdf" ).'</strong></td><td><strong>'.esc_html__( "Price", "wcorderpdf" ).'</strong></td></tr>';
 
-	        foreach( $order_items as $item_id => $order_item ) {
-				$html .= "<tr><td>".$order_item->get_name()."</td><td>".$order_item->get_quantity()."</td><td>".$currency." ".$order_item->get_total()."</td></tr>";
+			foreach( $order_items as $item_id => $order_item ) {
+				ob_start();
+				do_action( 'wcopd_order_pdf_add_order_item_meta', $order_item );
+				$meta .= ob_get_clean();
+				$html .= "<tr><td>".$order_item->get_name(). $meta . "</td><td>".$order_item->get_quantity()."</td><td>".$currency." ".$order_item->get_total()."</td></tr>";
 			}
 
 			$html .= '</table></td></tr>';

commit d3d4494d1277a0933785bb3f7b7a4a85022a09b3

fix: little bug, introduced in fe74761

includes/class-wc-order-pdf-download.php
@@ -166,7 +166,7 @@ class WC_Order_PDF_Download {
 			foreach( $order_items as $item_id => $order_item ) {
 				ob_start();
 				do_action( 'wcopd_order_pdf_add_order_item_meta', $order_item );
-				$meta .= ob_get_clean();
+				$meta = ob_get_clean();
 				$html .= "<tr><td>".$order_item->get_name(). $meta . "</td><td>".$order_item->get_quantity()."</td><td>".$currency." ".$order_item->get_total()."</td></tr>";
 			}
 